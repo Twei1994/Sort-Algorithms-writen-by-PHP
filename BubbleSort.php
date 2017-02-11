84c644aa09b30bd4eaaa8679b8ab4bc6d9dee354
唐伟 <tju213@gmail.com>
2017-02-11 20:29:02 +0800

    Add files via upload
    修改了Bubble.php

--- BubbleSort.php
+++ BubbleSort.php
@@ -1,28 +1,24 @@
 <?php 
 
 $list = array(9, 1, 5, 8, 3, 7, 4, 6, 2);
+BubbleSort($list);
+print_r($list);
 
-$length = count($list);			// 获取数组长度
-
-
-for ($i = 0; $i < $length; $i++) { 
-	for ($j = $length-1; $j > $i; $j--) { 
-		if ($list[$j-1] > $list[$j]) {
-			swap($list, $j-1, $j);
-			// print_r($list);
+function BubbleSort(&$list) {
+	$length = count($list);
+	for ($i = 0; $i < $length; $i++) { 
+		for ($j = $length-1; $j > $i; $j--) { 
+			if ($list[$j-1] > $list[$j]) {
+				swap($list, $j-1, $j);
+			}
 		}
-	}
 }
-
+}
 function swap(&$list, $i, $j)		// &表示传引用，同时改变变量的值
 {
 	$temp = $list[$i];
 	$list[$i] = $list[$j];
 	$list[$j] = $temp;
 	// return $list;
-}
-
-foreach ($list as $key => $value) {
-	echo "$value"."\n";
 }
 ?>
